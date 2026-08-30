5d266f1e708cd55c604ca63908d0e80edb1b8f78
Archit Prasad <165465776+archit7py@users.noreply.github.com>
2026-08-30 12:37:59 +0530

    Time: 0 ms (100.00%) | Memory: 29.3 MB (10.50%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         vector<int>res(n,-1);
         stack<int>st;
-        for(int i  = n-2;i>=0;i--){
+        for(int i = n-2;i>=0;i--){
             st.push(nums[i]);
         }
         for(int i=n-1;i>=0;i--){
