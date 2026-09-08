ead634481e8f387023c313efe19665b9a5a5c3c2
Archit Prasad <165465776+archit7py@users.noreply.github.com>
2026-09-08 20:55:37 +0530

    Time: 3 ms (1.08%) | Memory: 14.2 MB (49.61%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int low  = 0;
+        int low = 0;
         int high = nums.size()-1;
-        
         while(low<high){
             int guess = (low + high)/2;
-            if(nums[guess]>nums[high]){
+            if(nums[guess] > nums[high]){
                 low = guess+1;
+                
             }
-            else {
+            else{
                 high = guess;
-
+            
+                
             }
+
+
         }
         return nums[low];
         
