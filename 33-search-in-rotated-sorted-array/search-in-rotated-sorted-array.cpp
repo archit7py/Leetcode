29cb8270d5fa42faae5280c66bc0fe18d2c6b36b
Archit Prasad <165465776+archit7py@users.noreply.github.com>
2026-09-09 00:53:20 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (96.77%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,47 +1,47 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low  = 0;
+        int low = 0;
         int high = nums.size()-1;
+        int res = 0;
         while(low<=high){
             int guess = (low+high)/2;
             if(nums[guess] == target){
                 return guess;
+
             }
-            if(nums[guess]>nums[high]){
+            if(nums[guess] > nums[high]){
                 if(target > nums[guess]){
                     low = guess + 1;
                 }
                 else{
-                    if(nums[0]>target){
+                    if(target < nums[0]){
                         low = guess + 1;
                     }
                     else{
-                        high = guess-1;
+                        high = guess - 1;
                     }
-                    
                 }
             }
             else{
-                if(target<nums[guess]){
-                    high = guess - 1;
+                if(target < nums[guess]){
+                    high = guess -1;
                 }
                 else{
                     if(target > nums[high]){
-                        high = guess - 1;
-                    }
+                    high = guess - 1;
+                }
                     else{
                         low = guess + 1;
-                    }
-                    
                 }
 
-            }
-            
+                }
                 
-            
+            }
         }
         return -1;
+        
+
         
     }
 };
