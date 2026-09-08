db212733c73150e62f462ed0661776c205bd8b26
Archit Prasad <165465776+archit7py@users.noreply.github.com>
2026-09-08 13:43:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (13.44%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low < high) {
+            int guess = (low + high) / 2;
+
+            if (nums[guess] < nums[guess + 1]) {
+                low = guess + 1;
+            }
+            else {
+                high = guess;
+            }
+        }
+
+        return low;
+    }
+};
