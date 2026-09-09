36883ff9a89753ef30fed26863a8af579970eb4a
Archit Prasad <165465776+archit7py@users.noreply.github.com>
2026-09-09 20:36:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (73.88%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
-bool func(vector<int>&a,long long n, long long limit, long long stud){
-           long long k = 1;
-           long long page = 0;
-           for(int i = 0;i<n;i++){
-               if(page + a[i]<=limit){
-                   page += a[i];
-               }
-               else{
-                   k++;
-                   page = a[i];
-                   if(k>stud){
-                       return false;
-                   }
-                }
-               
-           }
-           return true;
-       }
-    int splitArray(vector<int>& nums, int k) {
-        long long n  = nums.size();
-        if(n<k){
-            return -1;
+int func(vector<int>&arr,int n, int limit, int k){
+    int a = 1;
+    int pos = 0;
+    for(int i = 0;i<n;i++){
+        if(pos + arr[i] <= limit){
+            pos += arr[i];
         }
-        long long low = 0;
-        long long high = 0;
+        else{
+            a++;
+            pos = arr[i];
+            if(a>k){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+    int splitArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        int low = 0;
+        int high = 0;
         for(int i = 0;i<n;i++){
-            low = max(low,(long long)nums[i]);
+            low = max(low,nums[i]);
             high += nums[i];
         }
-        long long res = -1;
+        int res = -1;
         while(low<=high){
-            long long guess = (low+high)/2;
+            int guess = (low+high)/2;
             if(func(nums,n,guess,k)){
                 res = guess;
                 high = guess - 1;
+            }else{
+                low = guess + 1;
             }
-            else{
-                low = guess + 1 ;
-            }
+            
         }
         return res;
         
