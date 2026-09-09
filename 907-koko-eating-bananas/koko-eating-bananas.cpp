019cb4af4edc8160439f1cfec3034b2d1ef2e608
Archit Prasad <165465776+archit7py@users.noreply.github.com>
2026-09-09 12:42:07 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (90.05%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-long long func(vector<int>&a,int n, int speed){
-    n = a.size();
+long long func(vector<int>&arr,int n ,int speed){
     long long h = 0;
     for(int i = 0;i<n;i++){
-        h += a[i]/speed;
-        if(a[i] % speed != 0){
+        h += arr[i]/speed;
+        if(arr[i] % speed != 0){
             h++;
         }
         
     }
     return h;
-};
-    int minEatingSpeed(vector<int>& piles, int h) {
-        long long max = piles[0];
-        for(int i = 0;i<piles.size();i++){
-            if(piles[i]>max){
-                max = piles[i];
-            }
-
-        }
+    
+}
+    int minEatingSpeed(vector<int>& piles, long long h) {
+        long long n = piles.size();
         long long low = 1;
-        int n = piles.size()-1;
-        long high = max;
+        long long high = 0;
+        for(int i = 0;i<n;i++){
+            high = max(high,(long long)piles[i]);
+        }
         long long res = -1;
         while(low<=high){
-            int guess = (low + high)/2;
-            long long hour = func(piles,n,guess);
-            if(hour > h){
+            long long guess = (low + high)/2;
+            long long ans = func(piles,n,guess);
+            if(ans > h){
                 low = guess + 1;
             }
             else{
