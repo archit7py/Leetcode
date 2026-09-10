af29f29fc19feae7b8765f3fb45cf8636f2e04a9
Archit Prasad <165465776+archit7py@users.noreply.github.com>
2026-09-10 21:01:52 +0530

    Time: 11 ms (25.01%) | Memory: 7.7 MB (81.47%) - LeetSync

--- 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.cpp
+++ 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
-int func(int n , int m , int g){
-    int row = n-1;
+int func(int m, int n,int guess){
+    int row = m-1;
     int col = 0;
-    int count = 0;
-    for(int i = 1;i<=m;i++){
-        count += min(g / i, n);
+    int count  = 0;
+    while(row >= 0 && col < n){
+        if((row + 1)*(col + 1)<=guess){
+            count += row + 1;
+            col++;
+
+        }
+        else{
+            row--; 
+        }
     }
-        
-    
     return count;
+    
 }
-
     int findKthNumber(int m, int n, int k) {
+        
         int low = 1;
-        int high = n*m;
+        int high = m*n;
         int res = -1;
-        while(low <= high){
-            int g = (low + high)/2;
-            int ans = func(n,m,g);
-            if(ans<k){
-                low = g + 1;
-
+        while(low<=high){
+            int guess = (low+high)/2;
+            int ans = func(m,n,guess);
+            if(ans < k){
+                low = guess + 1;
             }
             else{
-                res = g;
-                high = g - 1;
+                res = guess;
+                high = guess - 1;
             }
         }
         return res;
