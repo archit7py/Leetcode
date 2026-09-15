e24f1ce327954e2c8ef7d9ecfa4696fac1bb4d95
Archit Prasad <165465776+archit7py@users.noreply.github.com>
2026-09-15 19:43:05 +0530

    Time: 1797 ms (5.01%) | Memory: 459.1 MB (5.08%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,76 +1,49 @@
 class Solution {
 public:
-
-    struct cmp {
-        bool operator()(pair<char,int>& a, pair<char,int>& b) {
-            return a.second < b.second;
-        }
-    };
-
     int leastInterval(vector<char>& tasks, int n) {
-
         int s = tasks.size();
-        int seat = 1;
-        vector<int>res;
-
-        unordered_map<char,int> freq;
-        unordered_map<char,int> free;
-
-        for(int i = 0; i < s; i++) {
+        unordered_map<char,int>freq;
+        unordered_map<char,int>free;
+        for(int i=0;i<s;i++){
             freq[tasks[i]]++;
             free[tasks[i]] = 1;
         }
-
-        priority_queue<
-            pair<char,int>,
-            vector<pair<char,int>>,
-            cmp
-        > pq;
-
-        // First push ALL tasks
-        for(auto i : freq) {
-            pq.push({i.first, i.second});
+        priority_queue<pair<int,char>>pq;
+        for(auto i : freq){
+            pq.push({i.second,i.first});
         }
-
-        while(!pq.empty()) {
-
-            vector<pair<char,int>> pulled;
-
-            while(!pq.empty()) {
-
-                pair<char,int> p = pq.top();
+        int seat = 1;
+        while(!pq.empty()){
+            vector<pair<int,char>>pulled;
+            while(!pq.empty()){
+                pair<int,char>p = pq.top();
                 pq.pop();
-
-                char child = p.first;
-
-                // task is free to execute
-                if(free[child] <= seat) {
-
-                    res.push_back(child);
-
-                    p.second--;
-
-                    if(p.second > 0) {
-                        free[child] = seat + n + 1;
-                        pulled.push_back(p);
+                int freq = p.first;
+                char child = p.second;
+                if(free[child] <= seat){
+                    if(p.first>1){
+                        pq.push({p.first-1,p.second});
+                        free[p.second] = seat + n + 1;
+                        break;
+                        
                     }
-
-                    break;
+                    else{
+                        break;
+                    }
                 }
-
-                else {
+                else{
                     pulled.push_back(p);
                 }
             }
-
-            // Put tasks back
-            for(int i = 0; i < pulled.size(); i++) {
+            for(int i = 0;i<pulled.size();i++){
                 pq.push(pulled[i]);
             }
-
             seat++;
         }
+        return seat-1;
+          
+        
 
-        return seat - 1;
+        
     }
 };
