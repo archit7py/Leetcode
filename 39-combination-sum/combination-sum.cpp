e14b77bd84b4918859fc10573b3bbb755bf4c7ca
Archit Prasad <165465776+archit7py@users.noreply.github.com>
2026-09-20 22:50:26 +0530

    Time: 4 ms (36.31%) | Memory: 17.8 MB (23.75%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+void fun(vector<int>& a , int n, int idx, int sum, vector<int>& diary,vector<vector<int>>& res,int target){
+    if(sum == target){
+        res.push_back(diary);
+        return;
+}
+
+    if(idx == n || sum > target){
+        return;
+}
+    fun(a,n,idx+1,sum,diary,res,target);
+    if(sum + a[idx] <= target){
+        diary.push_back(a[idx]);
+        sum += a[idx];
+        fun(a,n,idx,sum,diary,res,target);
+        sum -= a[idx];
+        diary.pop_back();
+    }
+    return;
+}
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int sum = 0;
+        int n = candidates.size();
+        vector<int> diary;
+        vector<vector<int>> res;
+        int idx = 0;
+        fun(candidates, n, idx,sum,diary,res,target);
+        return res;
+        
+        
+    }
+};
